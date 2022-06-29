edc2d03908702e42a56efb4505bfc62d1367b937
kemalaziezrachmansyah <108319129+kemalaziezrachmansyah@users.noreply.github.com>
2022-06-29 17:09:15 +0900

    Update cpu_sa.cpp

--- neal/src/cpu_sa.cpp
+++ neal/src/cpu_sa.cpp
@@ -103,11 +103,11 @@ void simulated_annealing_run(
     bool flip_spin;
     int member_index;
     int other_index;
+    int base_index;
     double energydiff;
     char *statedummy;
     bool status;
-    int ind1;
-    int ind2;
+    int ind;
     
     
     // perform the sweeps
@@ -125,52 +125,52 @@ void simulated_annealing_run(
             const double threshold = 44.36142 / beta;
 
             for (int group_index = 0; group_index < num_vars/onehotpar; group_index++) {
-                ind1 = 0;
+                base_index = group_index*onehotpar;                
+                member_index = base_index;
+                
+                while (2*state[member_index] < 0) {
+                    member_index++;    
+                }
+                
+                ind = 0;
                 status = true;
                 
-                while (ind1 < onehotpar && status){
-                    member_index = group_index*onehotpar + ind1;
-                    ind2 = ind1 + 1;
+                while (ind < onehotpar && status){
+                    other_index = base_index + ind1;
                     
-                    while (ind2 < onehotpar && status){
-                        other_index = group_index*onehotpar + ind2;
-                    
-                        if (state[member_index] * state[other_index] < 0) {
-                            energydiff = get_flip_energy(member_index, state, h, degrees,
-                                                neighbors, neighbour_couplings);
-                            statedummy = state;
-                            statedummy[member_index] *= -1;
-                            energydiff += get_flip_energy(other_index, statedummy, h, degrees,
-                                                    neighbors, neighbour_couplings);
+                    if (member_index != other_index) {
+                        energydiff = get_flip_energy(member_index, state, h, degrees,
+                                            neighbors, neighbour_couplings);
+                        statedummy = state;
+                        statedummy[member_index] *= -1;
+                        energydiff += get_flip_energy(other_index, statedummy, h, degrees,
+                                               neighbors, neighbour_couplings);
 
-                            if (energydiff >= threshold) continue;
+                        if (energydiff >= threshold) continue;
 
-                            flip_spin = false;
+                        flip_spin = false;
 
-                            if (energydiff <= 0.0) {
-                                // automatically accept any flip that results in a lower 
-                                // energy
+                        if (energydiff <= 0.0) {
+                            // automatically accept any flip that results in a lower 
+                            // energy
+                            flip_spin = true;
+                        }
+                        else {
+                            // get a random number, storing it in rand
+                            FASTRAND(rand); 
+                            // accept the flip if exp(-delta_energy*beta) > random(0, 1)
+                            if (exp(-energydiff*beta) * RANDMAX > rand) {
                                 flip_spin = true;
                             }
-                            else {
-                                // get a random number, storing it in rand
-                                FASTRAND(rand); 
-                                // accept the flip if exp(-delta_energy*beta) > random(0, 1)
-                                if (exp(-energydiff*beta) * RANDMAX > rand) {
-                                    flip_spin = true;
-                                }
-                            }
-
-                            if (flip_spin) {
-                                state[member_index] *= -1;
-                                state[other_index] *= -1;
-                                status = false;
-                            }
                         }
-                        
-                        ind2++;
+
+                        if (flip_spin) {
+                            state[member_index] *= -1;
+                            state[other_index] *= -1;
+                            status = false;
+                        }
                     }
-                    ind1++;
+                    ind++;
                 }
             }
         }
