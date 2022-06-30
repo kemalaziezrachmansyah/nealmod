75ded6e421381c9fdb180e22c391cd88f21750c9
kemalaziezrachmansyah <108319129+kemalaziezrachmansyah@users.noreply.github.com>
2022-06-30 09:31:51 +0900

    Update sampler.py

--- neal/sampler.py
+++ neal/sampler.py
@@ -261,10 +261,7 @@ class SimulatedAnnealingSampler(dimod.Sampler, dimod.Initialized):
         num_reads = parsed.num_reads
 
         # read out the initial states and the variable order
-        initial_states_array = -1*np.ones((1,len(initial_states[0])), dtype='int8')
-        for i in range(len(initial_states[0]-1)):
-            if i % onehotpar == 0:
-                initial_states_array[0][i] = 1
+        initial_states_array = np.ascontiguousarray(initial_states)
         
         
         print('initial_states_array: ', initial_states_array)
